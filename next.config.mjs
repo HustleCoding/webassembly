8da737741d335c28263644ec58374ba95c73b521
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2024-07-20 22:58:50 +0300

    Fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
@@ -12,6 +18,9 @@ const nextConfig = {
       test: /\.wasm$/,
       type: "webassembly/async",
     });
+
+    // Add a resolver for the WebAssembly module
+    config.resolve.alias["@/wasm"] = path.resolve(__dirname, "rust-lib/pkg");
 
     return config;
   },
